e7e375cc4a3038ee14c8f03ee50f60ab3a784e43
Hector <HMoragues3@gmail.com>
2019-05-16 12:59:41 +0200

    MENU

--- app/src/main/java/com/flaiserapps/juanpalomo/MainActivity.java
+++ app/src/main/java/com/flaiserapps/juanpalomo/MainActivity.java
@@ -1,15 +1,77 @@
 package com.flaiserapps.juanpalomo;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private FrameLayout fl;
+    private Fragment recyclerRecetasFragment;
+    private FragmentTransaction transaction;
+    private FragmentManager fm;
+    private Toolbar toolbar;
+    private boolean isShown = false;
+    private LinearLayout menuLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=(Toolbar) findViewById(R.id.toolbar_menu);
+        setSupportActionBar(toolbar);
+
+        //menuLayout=(LinearLayout) findViewById(R.id.);
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
 
 
+    private void burgerMenuIconClick(){
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isShown) {
+                    menuLayout.setVisibility(View.VISIBLE);
+                    menuLayout.animate().translationY(menuLayout.getHeight());
+                    isShown = true;
+                } else {
+                    menuLayout.animate().translationY(menuLayout.getHeight() * (-1));
+                    isShown = false;
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
